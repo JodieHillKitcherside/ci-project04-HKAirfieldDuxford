898fb537deade266d221ff4300cf12b553c2b563
Jodie Hill-Kitcherside <jhillkitcherside@gmail.com>
2024-09-14 16:34:23 +0000

    Add UserRegistrationForm class to forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Booking, Payment
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm 
 
 class BookingForm(forms.ModelForm):
     date = forms.DateField(widget=forms.SelectDateWidget)
@@ -12,3 +14,16 @@ class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = ['amount', 'payment_method']
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = Userfields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True)
+    user = super(UserRegistrationForm, self).save(commit=False)
+    user.email = self.cleaned_data['email']
+    if commit:
+        user.save()
+    return user
