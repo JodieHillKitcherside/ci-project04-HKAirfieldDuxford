18c2909c134a95764777695f7ffee64f18f42a0e
Jodie Hill-Kitcherside <jhillkitcherside@gmail.com>
2024-09-14 14:34:41 +0000

    Add class Aircraft to models.py

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse 
+
+class Aircaft(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    capaicty = models.IntegerField()
+    cost_per_hour = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def _str_(self):
+        return self.name 
+
