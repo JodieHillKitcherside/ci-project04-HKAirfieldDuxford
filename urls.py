acec527c96040de6e03badeda31a80fe7db27e44
Jodie Hill-Kitcherside <jhillkitcherside@gmail.com>
2024-09-14 15:31:39 +0000

    Add urlpatterns to urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,10 @@
 from django.urls import path
+from . import views 
+
+urlpatterns - [
+    path('', views.home, name='home'),
+    path('flights/' views.flights_options, name='flight_options'),
+    path('booking/<int:flight_id>/' views.booking_view, name='booking'),
+    path('payment/<int:booking_id>/' views.payment_view, name='payment'),
+    path('confirmation/<int:booking_id>/' views.confirmation_view, name='confirmation'),
+]
