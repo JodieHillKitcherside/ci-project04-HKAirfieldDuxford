6e055ade0064d54a44d5ae792653e6ea3914aad9
Jodie Hill-Kitcherside <jhillkitcherside@gmail.com>
2024-09-14 14:50:00 +0000

    Add Payment class for models.py

--- models.py
+++ models.py
@@ -36,3 +36,12 @@ class Booking(models.Model):
     
     def _str_(self):
         return f"Booking {self.id} by {self.user.name}" 
+
+class Payment(models.Model):
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=50)
+
+    def _str_(self):
+        return f"Payment {self.id} for Booking {self.booking.id}"
