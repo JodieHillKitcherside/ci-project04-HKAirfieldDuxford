a001bab150b1a777b5ad30a3d280c316367d9b0f
Jodie Hill-Kitcherside <jhillkitcherside@gmail.com>
2024-09-14 14:58:29 +0000

    add classes BookingForm and PaymentForm to forms.py

--- forms.py
+++ forms.py
@@ -1,3 +1,13 @@
 from django import forms
 
-class BookingForm(forms.ModelForm)
+class BookingForm(forms.ModelForm):
+    date = forms.DateField(widget=forms.SelectDateWidget)
+
+    class Meta:
+        model = Booking
+        fields = ['flight', 'aircraft', 'date']
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = ['amount', 'payment_method']
